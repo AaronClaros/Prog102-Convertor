02e23c5c003b1e5e64c9d722d431bb0db410b759
Kevin Sanchez Delgadillo <KevinSanchez@Fundacion-jala.local>
2018-08-22 10:19:08 -0400

    empty class

---
+++ src/main/com/fundations/convertor/Search.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package main.com.fundations.convertor;
+/**
+ *  Search class for java applications, class which search videos
+ *
+ * @author Kevin Sanchez - AWT-[01].
+ * @version 0.1
+ */
+public class Search {
+    public Search() {
+    }
+    public int Found(){
+        
+        return 0;
+    }
+}
