c5d917698f5b6bdfa6b65b475b7ab8593bcd8e5b
Kevin Sanchez Delgadillo <KevinSanchez@Fundacion-jala.local>
2018-08-30 16:35:12 -0400

    criteria class fixed #2

--- src/main/java/com/foundations/convertor/common/Criteria.java
+++ src/main/java/com/foundations/convertor/common/Criteria.java
@@ -9,9 +9,6 @@
  * accordance with the terms of the license agreement you entered into
  * with Jala Foundation.
  *
- * This class implements log4j using Singleton
- * @author Angelica Lopez - AWT-[01].
- * @version 0.1
  */
 
 package com.foundations.convertor.common;
@@ -24,11 +21,14 @@ package com.foundations.convertor.common;
  */
 public class Criteria {
 
-    // path for the search
+    // path to search
     private String path;
 
-    // file name for the search
+    // file name to search
     private String fileName;
+
+    // extension of the file to search
+    private String extension;
 
     /**
      * Empty constructor
@@ -37,24 +37,44 @@ public class Criteria {
     }
 
     /**
-     * 
+     * @return  It returns the given path to search
      */
     public String getPath() {
-
         return path;
     }
 
+    /**
+    * @return It returns the given file to search
+    */
     public String getFileName() {
-
         return fileName;
     }
 
+    /**
+     * @return file's extension
+     */
+    public String getExtension() {
+        return extension;
+    }
+
+    /**
+     * @param path It will set the path for the search
+     */
     public void setPath(String path) {
         this.path = path;
     }
 
+    /**
+     * @param fileName It will set the file name for the search
+     */
     public void setFileName(String fileName) {
-
         this.fileName = fileName;
+    }
+
+    /**
+     * @param extension sets the extension of the file to search
+     */
+    public void setExtension(String extension) {
+        this.extension = extension;
     }
 }
