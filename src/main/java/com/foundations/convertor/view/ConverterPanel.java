4a13624749bcadad27fe35607d40f8c683c0f5cb
KevinHerrera <kevin031995@gmail.com>
2018-08-31 18:51:52 -0400

    comments on code

--- src/main/java/com/foundations/convertor/view/ConverterPanel.java
+++ src/main/java/com/foundations/convertor/view/ConverterPanel.java
@@ -17,6 +17,7 @@ package com.foundations.convertor.view;
 import javax.swing.*;
 import java.awt.*;
 
+
 /**
  *  JPanel Object that contains conversion parameters
  *
@@ -26,11 +27,19 @@ import java.awt.*;
 
 public class ConverterPanel extends JPanel {
     //label for title panel
-    JLabel labelPanelTitle;
-
-    JLabel labelOutPath;
-
-    JTextField fieldOutPath;
+    private JLabel labelPanelTitle;
+    //label for input path
+    private JLabel labelInputPath;
+    //text field for input path
+    private JTextField tFieldInputPath;
+    //button for get table selection
+    private JButton buttonGetTableSelect;
+    //label for output path
+    private JLabel labelOutPath;
+    //text field for output path
+    private JTextField tFieldOutPath;
+    //button for output path
+    private JButton buttonOutPath;
 
     /**
      * constructor of father class JPanel
@@ -64,23 +73,68 @@ public class ConverterPanel extends JPanel {
      * Initialize components
      */
     private void initComp() {
-        // instance labelPanelTitle and set text
+        //instance labelPanelTitle and set text
         labelPanelTitle = new JLabel("Conversion parameters");
-
-        labelPanelTitle.setBounds(10,10,150,25);
-
+        //instance labelInputPath and set text
+        labelInputPath = new JLabel("Input Path");
+        //instance textField for input path
+        tFieldInputPath = new JTextField();
+        //instance button get table selection and set text
+        buttonGetTableSelect = new JButton("Set Selection");
+        //instance label output path and set text
         labelOutPath = new JLabel("Output Path");
-
-        labelOutPath.setBounds(10, 50, 100, 25);
-
-        fieldOutPath = new JTextField();
-
-        fieldOutPath.setBounds(90,50,200,30);
-
+        //instance textField for OutputPath
+        tFieldOutPath = new JTextField();
+        //instance button for output path and set text
+        buttonOutPath = new JButton("Path");
+        //setting bounds
+        labelPanelTitle.setBounds(10,10,150,25);
+        labelInputPath.setBounds(10, 50, 100, 25);
+        tFieldInputPath.setBounds(90,50,200,25);
+        buttonGetTableSelect.setBounds(300,50,110,25);
+        labelOutPath.setBounds(430, 50, 100, 25);
+        tFieldOutPath.setBounds(520,50,200,25);
+        buttonOutPath.setBounds(730,50,60,25);
         // add components to panel
         this.add(labelPanelTitle);
+        this.add(labelInputPath);
+        this.add(tFieldInputPath);
+        this.add(buttonGetTableSelect);
+        this.add(buttonOutPath);
         this.add(labelOutPath);
-        this.add(fieldOutPath);
+        this.add(tFieldOutPath);
         this.setVisible(true);
+    }
+
+    /**
+     * getter method for text field input path
+     * @return text field input path
+     */
+    public JTextField getTFInputPath(){
+        return tFieldInputPath;
+    }
+
+    /**
+     * getter method for text field output path
+     * @return text field output path
+     */
+    public JTextField getTFOutputPath(){
+        return tFieldOutPath;
+    }
+
+    /**
+     * getter method for button get table selection
+     * @return text field input path
+     */
+    public JButton getBTableSelect(){
+        return buttonGetTableSelect;
+    }
+
+    /**
+     * getter method for button output path
+     * @return text field input path
+     */
+    public JButton getBOutputPath(){
+        return  buttonOutPath;
     }
 }
