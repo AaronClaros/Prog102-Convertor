9bc5a48f0e4587ee54ee9cd82c3cdf319e414392
anghelita <elianor@gmail.com>
2018-09-06 11:43:29 -0400

    Added comments and other tests to convert.

--- src/test/java/com/foundations/convertor/utils/ConverterUtilsTest.java
+++ src/test/java/com/foundations/convertor/utils/ConverterUtilsTest.java
@@ -1,14 +1,28 @@
+/*
+ * Copyright (c) 2018 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ * @author Kevin Sanchez - AWT-[01].
+ * @author Angelica Lopez - AWT-[01].
+ * @version 0.1
+ */
 package com.foundations.convertor.utils;
 
+import org.apache.commons.lang3.math.Fraction;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import java.sql.Timestamp;
-
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * ConverterUtilsTest class allows to test the convert data types
+ */
 public class ConverterUtilsTest {
 
     /**
@@ -37,6 +51,9 @@ public class ConverterUtilsTest {
         System.out.println(mockedConverter.timeToString(32760000L));
     }
 
+    /**
+     * this method validates the method doubleToTimeString from ConverterUtils class
+     */
     @Test
     public void doubleToTimeString(){
         ConverterUtils mockedConverter = mock(ConverterUtils.class);
@@ -45,10 +62,24 @@ public class ConverterUtilsTest {
         System.out.println(mockedConverter.doubleToTimeString(time));
     }
 
+    /**
+     * this method validates the method extensionToString from ConverterUtils class
+     */
     @Test
-    public void extension(){
+    public void extensionToString(){
         ConverterUtils mockedConverter = mock(ConverterUtils.class);
-        when(mockedConverter.extension(4,5)).thenReturn("4X5");
-        System.out.println(mockedConverter.extension(4,5));
+        when(mockedConverter.extensionToString(4,5)).thenReturn("4X5");
+        System.out.println(mockedConverter.extensionToString(4,5));
+    }
+
+    /**
+     * this method validates the method frameRateToString from ConverterUtils class
+     */
+    @Test
+    public void frameRateToString(){
+        Fraction fraction = Fraction.getFraction(25,1);
+        ConverterUtils mockedConverter = mock(ConverterUtils.class);
+        when(mockedConverter.frameRateToString(fraction)).thenReturn("25/1");
+        System.out.println(mockedConverter.frameRateToString(fraction));
     }
 }
