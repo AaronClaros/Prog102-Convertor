df514397357ce54a7bb85046e43ed13a0d991c81
Kevin <kevin.sanchezpeep@gmail.com>
2018-09-19 22:22:29 -0700

    unit test added

--- src/test/java/com/foundations/convertor/Model/Conversion/AudioConversionTest.java
+++ src/test/java/com/foundations/convertor/Model/Conversion/AudioConversionTest.java
@@ -1,5 +1,16 @@
+package com.foundations.convertor.Model.Conversion;
+import com.foundations.convertor.common.ConAudioCrit;
+import com.foundations.convertor.model.Conversion.AudioConversion;
+import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class AudioConversionTest {
-
+    @Test
+    public void testDoConversion() {
+        AudioConversion mockedAudio = mock(AudioConversion.class);
+        ConAudioCrit conAudioCrit = new ConAudioCrit();
+        mockedAudio.doConversion(conAudioCrit);
+        verify(mockedAudio).doConversion(conAudioCrit);
+    }
 }
