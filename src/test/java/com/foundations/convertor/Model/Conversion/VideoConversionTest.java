f88db36615c8c1fd0a1fc0116aa28a78b975cb4b
Kevin <kevin.sanchezpeep@gmail.com>
2018-09-23 11:50:56 -0700

    video conversion test code

--- src/test/java/com/foundations/convertor/Model/Conversion/VideoConversionTest.java
+++ src/test/java/com/foundations/convertor/Model/Conversion/VideoConversionTest.java
@@ -1,5 +1,20 @@
+package com.foundations.convertor.Model.Conversion;
+
+import com.foundations.convertor.common.ConversionVideoCriteria;
+import com.foundations.convertor.model.Conversion.VideoConversion;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class VideoConversionTest {
 
+    @Test
+    public void testDoConversion() {
+        VideoConversion mockedVideo = mock(VideoConversion.class);
+        ConversionVideoCriteria conversionVideoCriteria = new ConversionVideoCriteria();
+        mockedVideo.doConversion(conversionVideoCriteria);
+        verify(mockedVideo).doConversion(conversionVideoCriteria);
+    }
 }
