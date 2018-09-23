72fed911bb16a4226b2b92db63465018221f9f81
Kevin <kevin.sanchezpeep@gmail.com>
2018-09-23 15:26:16 -0700

    unit tests

--- src/test/java/com/foundations/convertor/Model/Conversion/AudioConversionTest.java
+++ src/test/java/com/foundations/convertor/Model/Conversion/AudioConversionTest.java
@@ -6,6 +6,16 @@ import static org.mockito.Mockito.*;
 
 public class AudioConversionTest {
     @Test
+    public void testDoConversionException() {
+        AudioConversion mockedAudio = mock(AudioConversion.class);
+        ConversionAudioCriteria conversionAudioCriteria = new ConversionAudioCriteria();
+        conversionAudioCriteria.setBitDepth("s16");
+        conversionAudioCriteria.setAudioCodec("mp3");
+        mockedAudio.doConversion(conversionAudioCriteria);
+        doThrow(new RuntimeException()).when(mockedAudio).doConversion(conversionAudioCriteria);
+    }
+
+    @Test
     public void testDoConversion() {
         AudioConversion mockedAudio = mock(AudioConversion.class);
         ConversionAudioCriteria conversionAudioCriteria = new ConversionAudioCriteria();
--- src/test/java/com/foundations/convertor/Model/Conversion/VideoConversionTest.java
+++ src/test/java/com/foundations/convertor/Model/Conversion/VideoConversionTest.java
@@ -5,16 +5,25 @@ import com.foundations.convertor.model.Conversion.VideoConversion;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class VideoConversionTest {
 
     @Test
-    public void testDoConversion() {
+    public void testDoConversionException() {
         VideoConversion mockedVideo = mock(VideoConversion.class);
         ConversionVideoCriteria conversionVideoCriteria = new ConversionVideoCriteria();
         mockedVideo.doConversion(conversionVideoCriteria);
-        verify(mockedVideo).doConversion(conversionVideoCriteria);
+        doThrow(new RuntimeException()).when(mockedVideo).doConversion(conversionVideoCriteria);
+    }
+
+    @Test
+    public void testDoConversion() {
+        VideoConversion mockedVideo = mock(VideoConversion.class);
+        ConversionVideoCriteria conversionAudioCriteria = new ConversionVideoCriteria();
+        mockedVideo.doConversion(conversionAudioCriteria);
+        verify(mockedVideo).doConversion(conversionAudioCriteria);
     }
 }
